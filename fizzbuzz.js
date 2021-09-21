944a29ba5d299119aaff4f600b951a4cdb091ffd
Alexandru Maxim <15928925+tutyamxx@users.noreply.github.com>
2021-09-21 15:10:02 +0100

    Update for noobs

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,4 +1,22 @@
+// --| Fastest way and less code (and my favourite)
+console.time("First");
 for (let i = 1; i <= 100; i++)
 {
-   console.log(((i % 3) ? "" : "Fizz") + ((i % 5) ? "" : "Buzz") || i);
+    console.log(((i % 3) ? "" : "Fizz") + ((i % 5) ? "" : "Buzz") || i);
 }
+console.timeEnd("First"); // --| Average of: 20-22.753ms
+
+
+
+// --| Not so fast way and more code
+console.time("Second");
+for (let i = 1; i <= 100; i++)
+{
+    let output = "";
+
+    if (i % 3 === 0) output += "Fizz";
+    else if (i % 5 === 0) output += "Buzz";
+
+    console.log(output || i);
+}
+console.timeEnd("Second"); // --| Average of: 23-24.305ms
